43306004a18e4533a04ffede7b7a856f2fe40cfb
levinoi <levi.alya@gmail.com>
2025-08-15 12:42:34 +0200

    fix negative test

--- src/test/java/com/phonebook/tests/AddContactNegativeTests.java
+++ src/test/java/com/phonebook/tests/AddContactNegativeTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AddContactNegativeTests extends TestBase {
-
+    @BeforeMethod
     public void precondition() {
         if (!app.getUser().isLoginLinkPresent()) {
             app.getUser().clickOnSignOutButton();
